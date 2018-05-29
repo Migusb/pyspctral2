a0240de759881826c0b14f55146b85cfe1c6caa2
Zachary Moon <zlm1@psu.edu>
2018-05-29 14:38:32 -0400

    C code template now in separate file

---
+++ C_code/run_spctral2_template.c
@@ -0,0 +1,92 @@
+
+/* 
+This is a C code template for use with Python string format
+
+As such, curly braces are doubled,
+in places where curly brace is intended within the C code
+
+
+*/
+
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "spectrl2.h"   /* <-- include for spectrl2.  */
+
+
+int main ( )
+{{
+        struct specdattype spdat, *specdat;  /* spectral2 data structure */
+        
+        /* point to the specral2 structure */
+        specdat = &spdat;
+        
+        /* Initialize the data structure -- you can omit if ALL structure vaiables 
+ *                         are assigned elswhere. Defaults in initialization can be overridden by 
+ *                                                 reassigning below. */
+           
+        S_spec_init (specdat);
+    
+    /*  */
+    
+    specdat->longitude = {lon:.4f};  /* Note that latitude and longitude are  */
+    specdat->latitude  = {lat:.4f};  /*   in DECIMAL DEGREES, not Deg/Min/Sec */
+    specdat->timezone  = {utcoffset: .1f};   /* -5: Eastern time zone, even though longitude would
+                                                suggest Central.  We use what they use.
+                                                DO NOT ADJUST FOR DAYLIGHT SAVINGS TIME. */
+
+    /*  */
+    
+    specdat->year      = {year:d};
+    specdat->month     = {month:d};
+    specdat->day       = {day:d};
+
+    /*  */
+
+    specdat->hour      = {hour:d};
+    specdat->minute    = {minute:d};
+    specdat->second    = {second:d};
+
+    /* Let's assume that the temperature is 27 degrees C and that
+ *        the pressure is 1006 millibars.  The temperature is used for the
+ *               atmospheric refraction correction, and the pressure is used for the
+ *                      refraction correction and the pressure-corrected airmass. */
+
+    specdat->temp      = {temp:.2f};
+    specdat->press     = {press:.2f};
+
+    /* We will use the first set of units (energy vs wavelength) */
+    
+    specdat->units     = 1;
+    
+    /* Tau at 500 nm will be assumed to be 0.2, and we will assume 1.36 cm of
+ *        atmospheric water vapor in a vertical column. */
+
+    specdat->tau500    = 0.2;
+    specdat->watvap    = 1.36;
+    specdat->ozone     = -1.0;  /* -1.0 input forces an internal calculation */
+
+    /* Finally, we will assume that you have a flat surface facing southeast,
+ *        tilted at latitude. */
+
+    specdat->tilt      = specdat->latitude;  /* Tilted at latitude */
+    specdat->aspect    = 180.0;       /* 0 deg. = facing north; 180 deg. = facing south */
+
+    /* run the model */
+    S_spectral2 ( specdat );
+
+    /*printf ( "micron  direct  diffuse\n" );*/
+    int i;
+    for (i = 0; i < 122; i++) {{
+
+        printf ( "%.3e,%.6e,%.6e\n", 
+            specdat->specx[i], specdat->specdir[i], specdat->specdif[i] );
+
+    }}
+
+
+        return ( 0 );
+}}
+
+
--- pyspctral2.py
+++ pyspctral2.py
@@ -13,91 +13,8 @@ from subprocess import call
 import numpy as np
 
 
-C_template=r"""
-#include <math.h>
-#include <string.h>
-#include <stdio.h>
-
-#include "spectrl2.h"   /* <-- include for spectrl2.  */
-
-
-int main ( )
-{{
-        struct specdattype spdat, *specdat;  /* spectral2 data structure */
-        
-        /* point to the specral2 structure */
-        specdat = &spdat;
-        
-        /* Initialize the data structure -- you can omit if ALL structure vaiables 
-                        are assigned elswhere. Defaults in initialization can be overridden by 
-                        reassigning below. */
-           
-        S_spec_init (specdat);
-    
-    /*  */
-    
-    specdat->longitude = {lon:.4f};  /* Note that latitude and longitude are  */
-    specdat->latitude  = {lat:.4f};  /*   in DECIMAL DEGREES, not Deg/Min/Sec */
-    specdat->timezone  =  -5.0;   /* Eastern time zone, even though longitude would
-                                                suggest Central.  We use what they use.
-                                                DO NOT ADJUST FOR DAYLIGHT SAVINGS TIME. */
-
-    /*  */
-    
-    specdat->year      = {year:d};
-    specdat->month     = {month:d};
-    specdat->day       = {day:d};
-
-    /*  */
-
-    specdat->hour      = {hour:d};
-    specdat->minute    = {minute:d};
-    specdat->second    = {second:d};
-
-    /* Let's assume that the temperature is 27 degrees C and that
-       the pressure is 1006 millibars.  The temperature is used for the
-       atmospheric refraction correction, and the pressure is used for the
-       refraction correction and the pressure-corrected airmass. */
-
-    specdat->temp      = {temp:.2f};
-    specdat->press     = {press:.2f};
-
-    /* We will use the first set of units (energy vs wavelength) */
-    
-    specdat->units     = 1;
-    
-    /* Tau at 500 nm will be assumed to be 0.2, and we will assume 1.36 cm of
-       atmospheric water vapor in a vertical column. */
-       
-    specdat->tau500    = 0.2;
-    specdat->watvap    = 1.36;
-    specdat->ozone     = -1.0;  /* -1.0 input forces an internal calculation */
-
-    /* Finally, we will assume that you have a flat surface facing southeast,
-       tilted at latitude. */
-
-    specdat->tilt      = specdat->latitude;  /* Tilted at latitude */
-    specdat->aspect    = 180.0;       /* 0 deg. = facing north; 180 deg. = facing south */
-
-    /* run the model */
-    S_spectral2 ( specdat );
-
-    /* after modifying the model code to save SZA, print it out here too */
-    /*printf ( "%.4f", specdat->zenref )*/
-
-    /*printf ( "micron  direct  diffuse\n" );*/
-    int i;
-    for (i = 0; i < 122; i++) {{
-
-        printf ( "%.3e,%.6e,%.6e\n", 
-            specdat->specx[i], specdat->specdir[i], specdat->specdif[i] );
-
-    }}
-
-
-        return ( 0 );
-}}
-"""
+with open('./C_code/run_spctral2_template.c', 'r') as f:
+    C_template = f.readlines()
 
 
 class spctral2():
@@ -106,6 +23,7 @@ class spctral2():
         lat=45.0, lon=-80.0, 
         year=2014, month=7, day=1,
         hour=12, minute=0, second=0,
+        utcoffset=-5.,
         temp=20, press=1000,
         casename='test', ID='001', 
         )
@@ -120,8 +38,8 @@ class spctral2():
         self.minute = minute
         self.second = second
         
-        self.temp = temp
-        self.press = press
+        self.temp = temp  # near-sfc air temp (deg. C)
+        self.press = press  # near-sfc air pressure (mb)
 
         # ozone, water, etc
 
@@ -143,10 +61,11 @@ class spctral2():
         """ """
         
         #> create C code
-        C_code = C_template.format(lon=lon, lat=lat,
-                year=year, month=month, day=day,
-                hour=hour, minute=minute, second=second,
-                temp=Tair_C, press=p_mb)
+        C_code = C_template.format(lon=self.lon, lat=self.lat,
+                year=self.year, month=self.month, day=self.day,
+                hour=self.hour, minute=self.minute, second=self.second,
+                utcoffset=self.utcoffset,
+                temp=self.temp, press=self.press)
         #print C_code
 
         cfname = 'tmp.c'
